1636dfabadd3f5b985341931e38d069708f89c9b
blkopsdev <blkopsdev@gmail.com>
2019-02-08 13:01:07 -0500

    modified:   blog/content/default-content.php

--- blog/content/default-content.php
+++ blog/content/default-content.php
@@ -3,13 +3,7 @@
 <div class="blog-title-box">
 	<a href="<?php echo esc_url(get_permalink()); ?>"><h3><?php the_title(); ?></h3></a>
 	</div>	
-	<div class="blog-meta-info">
-		<div class="blog-meta-info-inner">
-			<span><?php echo esc_html_e('Posted on','invictus'); ?></span> 
-			<span class="blog-meta-date"><?php echo get_the_date(); ?></span>
-			<span><?php echo esc_html_e('In','invictus'); ?> <?php the_category(', '); ?></span>
-		</div>
-	</div>	
+	
 		<div class="blog-text-box">
 		<?php the_excerpt(); ?></div>
 
