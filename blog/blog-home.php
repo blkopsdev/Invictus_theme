8b7f18a42b678084b2986863dec70e9c8df5a1ea
blkopsdev <blkopsdev@gmail.com>
2019-02-10 11:54:39 +0000

    added latest post title and author as a homepage title

--- blog/blog-home.php
+++ blog/blog-home.php
@@ -6,59 +6,62 @@ Template Name: No Header footer
 
 <?php get_header();
 global $ins_opt;
+ 
+$archive_year  = get_the_time('Y'); 
+$archive_month = get_the_time('m'); 
+$archive_day   = get_the_time('d'); 
+
  ?>
 <section id="content" role="main">
 <?php 
-if ( has_post_thumbnail($post->ID) ) { 
-	echo get_the_post_thumbnail_url() . "<br>";
-  	$single_blog_bg =   'style="background-image: url('.get_the_post_thumbnail_url().');"'; 
-
-}else{
-	$single_blog_bg =   ''; 
-}
     $args = array( 'numberposts' => '1', 'category' => CAT_ID );
     $recent_posts = wp_get_recent_posts( $args );
     foreach( $recent_posts as $recent ){
-    	$post_id = $recent->ID;
+    	$post_id = $recent['ID'];
+        
     	$category = get_the_category($post_id);
-    	echo $post_id . "<br>";
-    	print_r($category);
+        $category_name = $category[0]->name;
+        if ( has_post_thumbnail($post_id) ) { 
+            $single_blog_bg =   'style="background-image: url('.get_the_post_thumbnail_url($post_id).');"'; 
+
+        }else{
+            $single_blog_bg =   ''; 
+        }
 
 ?>
-<!-- <div class="inv-single-blog-title-wrapper text-center">
-    <div class="inv-single-blog-title-bg" <?php // echo wp_kses_post($single_blog_bg); ?>></div>
+<div class="inv-single-blog-title-wrapper text-center">
+    <div class="inv-single-blog-title-bg" <?php  echo wp_kses_post($single_blog_bg); ?>></div>
     <div class="inv-single-blog-title-bg-overlay"></div>
     <div class="inv-single-blog-under-header-wrapper" style="height: 80px;"></div>
     <div class="inv-single-blog-title-inner">
         <div class="container custom-blog-title">
             <div class="inv-single-blog-category-row">
-                <div class="inv-single-blog-subtitle">
-                    <?php // the_category(', '); ?>
+                <div class="inv-single-blog-subtitle" style="color: #aaaaaa; font-weight: bold; font-size: 14px; line-height: 28px; letter-spacing: 1px; display: inline-block; margin: 0 14px; text-transform: uppercase;">
+                    <?php echo $category_name; ?>
                 </div>
             </div>
 
             <div class="inv-single-blog-title-row">
-                <h1 class="inv-single-blog-title">
-                    <?php // the_title(); ?>
-                </h1>
+                <a href="<?php echo $recent['guid'] ?>"><h1 class="inv-single-blog-title">
+                    <?php echo $recent['post_title']; ?>
+                </h1></a>
             </div>
 
             <div class="inv-single-blog-meta-row">
                 <div class="inv-single-blog-meta-text">
-                    <div class="inv-single-blog-author">
-                        <?php // the_author_posts_link(); ?>
+                    <div class="inv-single-blog-author" style="color: #aaaaaa; font-weight: bold; font-size: 14px; line-height: 28px; letter-spacing: 1px; display: inline-block; margin: 0 14px; text-transform: uppercase;">
+                        <?php
+                            echo get_the_author_meta( 'display_name' , $recent['post_author'] );
+                        ?>
                     </div>
                 </div>
-                <div class="inv-single-blog-date inv-single-blog-meta-text">
-                    <time datetime="2016-11-07T16:24:47+00:00"><a href="<?php // echo get_day_link( $archive_year, $archive_month, $archive_day); ?>">
-                            On <?php // the_date('F d, Y'); ?></a></time>
-                </div>
+                
 
 
             </div>
         </div>
     </div>
-</div> -->
+</div>
 <?php
 
     }
